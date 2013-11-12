1e01fa048d389e2119acc07cc3260a0ff54f5e99
mikegmoll <mike.g.moll@gmail.com>
2013-11-11 23:42:05 -0800

    Create pinShamz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinshambz::Application.config.secret_key_base = 'a7828f6da4fa9cf7fce4b9f6edf97b831a464d9ffada8ba5cf9e3549ccfd93e17196753ed772c191a731dfb628ad9768a4560f16da416bd3e27d2ebd7c2ec447'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinshambz::Application.config.session_store :cookie_store, key: '_pinshambz_session'
