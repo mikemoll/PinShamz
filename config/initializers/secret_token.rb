87b0924fd0ff5806faa49ffae666ddfdec091579
mikegmoll <mike.g.moll@gmail.com>
2013-12-30 14:17:04 -0800

    bundle update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pinshambz::Application.config.secret_key_base = 'a7828f6da4fa9cf7fce4b9f6edf97b831a464d9ffada8ba5cf9e3549ccfd93e17196753ed772c191a731dfb628ad9768a4560f16da416bd3e27d2ebd7c2ec447'
+Pinshambz::Application.config.secret_key_base = '168cfe76d23290ec15162606a4306df6ab1a0567db006804fcdebd581de8c1e9ce51ff2f84a471b84ccf4fa1fc81a192613cce52d7ec1841652f5f792dd04b7f'
